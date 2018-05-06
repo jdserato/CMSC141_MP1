6af96dce1e6e3bd8e6298d2c1f02772fbc1ffa8b
Jay Vince Serato <jdserato@up.edu.ph>
2018-05-06 20:34:22 +0800

    Submittable yet issues remain unsolved.

--- src/MPA2/Main.java
+++ src/MPA2/Main.java
@@ -1,9 +1,6 @@
 package MPA2;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,16 +9,17 @@ import java.util.List;
  */
 public class Main {
     private static final File INPUT = new File("mp3.in");
-    private static Regex root;
+    private static final File OUTPUT = new File("AlvarezFamatSerato.out");
 
     public static void main(String[] args) {
         BufferedReader br = null;
+        BufferedWriter bw = null;
         try {
             br = new BufferedReader(new FileReader(INPUT));
+            bw = new BufferedWriter(new FileWriter(OUTPUT));
             int numberOfTestCases = Integer.parseInt(br.readLine()); // First line is defined to be the total number of test cases.
 
             while (numberOfTestCases-- > 0) {
-                // TODO get regex and fragment them.
                 String regexRead = br.readLine(); // The regular expression (regex) is defined here.
                 regexRead = "(".concat(regexRead).concat(")");
                 String newRegexRead = "";
@@ -30,14 +28,11 @@ public class Main {
                         newRegexRead = newRegexRead.concat(regexRead.charAt(i) + "");
                     }
                 }
-                root = new Regex(newRegexRead);
+                Regex root = new Regex(newRegexRead);
                 root = fragment(root, newRegexRead).get(0);
-                System.out.println(root + " is root.");
 
-                // TODO get the number of sample expressions
                 int numberOfSampleExp = Integer.parseInt(br.readLine());
 
-                // TODO identification of expressions
                 while (numberOfSampleExp-- > 0) {
                     boolean accepted = true;
                     String testCase = br.readLine();
@@ -47,9 +42,7 @@ public class Main {
                     Regex current = root;
                     current = current.clearAllDoneStatus(current);
                     while (testCase.length() > 0) {
-                        System.out.println("Testcase: " + testCase);
                         while (current.getChildren() != null) {
-                            System.out.println(current.getChildren().get(0) + "'s current inspected num: " + current.getChildrenInsp());
                             if (!current.isUnion()) {
                                 current.setChildrenInsp(current.getChildrenInsp() + 1);
                             }
@@ -62,13 +55,11 @@ public class Main {
                                 current = current.getChildren().get(current.getChildrenInsp() - 1);
                             }
                         }
-                        System.out.println("Current node we're at: " + current.getContents().get(0));
                         int maxLen = 0;
                         String partial;
                         if (current.getParent() != null && current.getParent().isUnion()) { // it's a union
                             //current = current.getParent();
                             for (String c : current.getContents()) {
-                                System.out.println("Content: " + c);
                                 if (maxLen < c.length()) {
                                     maxLen = c.length();
                                 }
@@ -81,10 +72,8 @@ public class Main {
                         } else {
                             partial = testCase.substring(0, maxLen);
                         }
-                        System.out.println("Inspecting the partial testcase : " + partial);
                         boolean matched = false;
                         for (String s : current.getContents()) {
-                            System.out.println("Equalizing " + s + " and " + partial);
                             if (partial.equals(s)) {
                                 matched = true;
                                 break;
@@ -95,9 +84,7 @@ public class Main {
                                 current.setDone(true);
                             }
                             if (!matched) {
-                                System.out.println("Warning, not matched.");
                                 if (current.getParent() != null && current.getParent().isUnion()) {
-                                    System.out.println("Exiting union.");
                                     current = current.getParent();
                                     current.setDone(true);
                                 }
@@ -111,7 +98,6 @@ public class Main {
                         }
                         if (matched) {
                             testCase = testCase.substring(maxLen);
-                            System.out.println("Matched and we shall continue.");
                         }
                     }
                     //current = root;
@@ -134,18 +120,23 @@ public class Main {
                         }
                     }
                     if (!accepted) {
-                        System.out.println("NO");
+                        bw.append("no");
+                        bw.newLine();
                     } else {
-                        System.out.println("YE");
+                        bw.append("yes");
+                        bw.newLine();
                     }
                 }
             }
         } catch (IOException e) {
-            System.out.println("File not found.");
+            System.err.println("File not found.");
         } finally {
             try {
                 if (br != null) {
                     br.close();
+                }
+                if (bw != null) {
+                    bw.close();
                 }
             } catch (IOException e) {
                 // do nothing
@@ -154,7 +145,6 @@ public class Main {
     }
 
     static List<Regex> fragment(Regex src, String content) {
-        System.out.println("Content is " + content);
         List<Regex> children = new ArrayList<>();
         List<String> subcontent = new ArrayList<>();
         int parentheses = 0;
@@ -162,37 +152,29 @@ public class Main {
         String current = "";
         for (int i = 0; i < content.length(); i++) { // Inspecting each and every character.
             char c = content.charAt(i);
-            System.out.println("Scanning " + c);
             if (c == '(') { // Mapping out first-level parentheses.
                 parentheses++;
                 if (parentheses > 1) { // If it is not a first-level, save it for the next fragmentation.
                     current = current.concat(c + "");
-                    System.out.println("New current at " + current);
                 }
             } else if (c == ')') {
                 parentheses--;
                 if (parentheses >= 1) { // If it is not a first-level, save it for the next fragmentation.
                     current = current.concat(c + "");
-                    System.out.println("New current at " + current);
                 }
             } else if (!(parentheses == 0 && (c == 'U'|| c == '*'))){ // Save everything else.
                 current = current.concat(c + "");
-                System.out.println("New current at " + current);
             }
             if (parentheses == 0 || i == content.length() - 1) { // First-level inspection.
-                System.out.println(c + " entered holy land.");
                 if (i + 1 < content.length() && content.charAt(i + 1) == 'U') { // If union is detected,...
                     subcontent.add(current);
-                    System.out.println("Unioned " + current);
                     current = "";
                     if (!unioned) {
                         unioned = true;
                     }
                 } else if (unioned && i == content.length() - 1) { // If it has been unioned but it is the last character
                     subcontent.add(current);
-                    System.out.println("Unioned " + current);
                 } else {
-                    // FIXME this should make sense to all of the following: a*b*, ab*, (ab) and abc*
                     if (i + 1 < content.length() && content.charAt(i + 1) == '*') {
                         i++;
                         if (c != ')') {
@@ -201,7 +183,6 @@ public class Main {
                             if (current.length() != 1) {
                                 sub.add(subs);
                                 children.add(new Regex(sub, false, src));
-                                System.out.println("ADDED " + subs + " at false.");
                             }
                             if (subs.contains("*") || subs.contains("U") || subs.contains("(")) {
                                 children.get(children.size() - 1).setChildren(fragment(children.get(children.size() - 1), subs));
@@ -210,26 +191,21 @@ public class Main {
                             subs = current.substring(current.length() - 1);
                             sub.add(subs);
                             children.add(new Regex(sub, true, src));
-                            System.out.println("ADDED " + subs);
                             if (subs.contains("*") || subs.contains("U") || subs.contains("(")) {
                                 children.get(children.size() - 1).setChildren(fragment(children.get(children.size() - 1), subs));
                             }
                         } else {
-                            System.out.println("Multi presence detected. Ratifying " + current);
                             subcontent.add(current);
                             children.add(new Regex(subcontent, true, src));
                             if (current.contains("*") || current.contains("U") || current.contains("(")) {
-                                System.out.println("Further simplifying...");
                                 children.get(children.size() - 1).setChildren(fragment(children.get(children.size() - 1), current));
                             }
                         }
                         subcontent = new ArrayList<>();
                         current = "";
                     } else if (i + 1 == content.length() || content.charAt(i + 1) == '('){
-                        System.out.println("Found EOF or open parenthesis. Current value: " + current);
                         subcontent.add(current);
                         children.add(new Regex(subcontent, false, src));
-                        System.out.println(children + " is children.");
                         if (current.contains("*") || current.contains("U") || current.contains("(")) {
                             children.get(children.size() - 1).setChildren(fragment(children.get(children.size() - 1), current));
                         }
@@ -238,14 +214,10 @@ public class Main {
                     }
                 }
             }
-            System.out.println("Old current at " + current);
-            System.out.println(children + " is children.");
         }
         if (!subcontent.isEmpty()) {
-            System.out.println("Subcontent is empty.");
             children.add(new Regex(subcontent, false, src));
         }
-        System.out.println(children + " is children.");
         return children;
     }
 }
